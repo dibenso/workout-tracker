18123b8915b309bfb3089278564052326af674ff
Dillon Benson <dibenso93@gmail.com>
2021-03-15 13:53:03 -0400

    setup Workout model

---
+++ models/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const ExerciseSchema = new mongoose.Schema({
+  type: "String",
+  name: "String",
+  duration: "Number",
+  weight: "Number",
+  reps: "Number",
+  sets: "Number"
+});
+const WorkoutSchema = new mongoose.Schema({ day: Date, exercises: [ExerciseSchema] });
+const Workout = new mongoose.model("Workout", WorkoutSchema);
+
+module.exports = { Workout };
