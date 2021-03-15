6a1c70eca1ac34d2c4c384030367e4ebb8d2fd96
Dillon Benson <dibenso93@gmail.com>
2021-03-15 17:28:47 -0400

    create route to get all workouts within the past 7 days

--- routes/index.js
+++ routes/index.js
@@ -19,22 +19,6 @@ router.get("/api/workouts", async (req, res) => {
   }
 });
 
-router.get("/api/workouts/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const workout = await Workout.findById(id).exec();
-
-    if(workout)
-      res.json(workout);
-    else
-      res.sendStatus(404);
-  } catch(error) {
-    res.sendStatus(500);
-
-    throw error;
-  }
-})
-
 router.put("/api/workouts/:id", async (req, res) => {
   try {
     const id = req.params.id;
@@ -53,10 +37,42 @@ router.put("/api/workouts/:id", async (req, res) => {
 
 router.post("/api/workouts", async (req, res) => {
   try {
-    const workout = new Workout({ day: new Date(new Date().setDate(new Date().getDate())) });
+    const workout = new Workout({ day: new Date().setDate(new Date().getDate()) });
     await workout.save();
 
     res.json(workout);
+  } catch(error) {
+    res.sendStatus(500);
+
+    throw error;
+  }
+});
+
+router.get("/api/workouts/range", async (req, res) => {
+  try {
+    const workouts = await Workout.find({
+      day: {
+        $gte: new Date(new Date() - 7 * 60 * 60 * 24 * 1000)
+      }
+    }).exec();
+
+    res.json(workouts);
+  } catch(error) {
+    res.sendStatus(500);
+
+    throw error;
+  }
+});
+
+router.get("/api/workouts/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const workout = await Workout.findById(id);
+
+    if(workout)
+      res.json(workout);
+    else
+      res.sendStatus(404);
   } catch(error) {
     res.sendStatus(500);
 
