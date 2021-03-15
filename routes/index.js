489cf06f5fc1d4510d0fe5b35630e33914036b86
Dillon Benson <dibenso93@gmail.com>
2021-03-15 14:35:14 -0400

    create route to respond to /api/workouts/:id with a single workout

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,20 @@ router.get("/api/workouts", async (req, res) => {
   }
 });
 
+router.get("/api/workouts/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const workout = await Workout.findById(id).exec();
+
+    if(workout)
+      res.json(workout);
+    else
+      res.sendStatus(404);
+  } catch(error) {
+    throw error;
+  }
+})
+
 router.put("/api/workouts/:id", (req, res) => {
   const id = req.params.id;
 
